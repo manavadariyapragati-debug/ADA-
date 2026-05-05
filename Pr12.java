0773c4e750f6eb4572933b000b67223391cac8ac
manavadariyapragati-debug <manavadariyapragati@gmail.com>
2026-05-05 22:35:43 +0530

    Pr12

---
+++ Pr12.java
@@ -0,0 +1,30 @@
+public class Pr12 {
+    public static void main(String[] args) {
+        String X = "AGGTAB";
+        String Y = "GXTXAYB";
+
+        int m = X.length();
+        int n = Y.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println("Length of LCS: " + dp[m][n]);
+    }
+}
+
+/*
+Output:
+Length of LCS: 4
+*/
